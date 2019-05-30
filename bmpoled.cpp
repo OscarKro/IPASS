7672d288b95b989c746911e4e57d3172bfe07612
OscarKromhout <oscarkromhout@gmail.com>
2019-05-30 15:49:34 +0200

    begin bmp toegevoegd

--- bmpoled.cpp
+++ bmpoled.cpp
@@ -1,10 +1,17 @@
 #include "bmpoled.hpp"
-
+//bmp slave adress = 0b1110110 (0x76)
 bmpoled::bmpoled(hwlib::target::pin_oc sclOled, hwlib::target::pin_oc sdaOled)
-    : sclOled(sclOled), sdaOled(sdaOled), busOled(sclOled, sdaOled), display(busOled, 0x3c), window(display, font) {}
+    : sclOled(sclOled), sdaOled(sdaOled), bus(sclOled, sdaOled) ,display(bus, 0x3c), window(display, font) {}
 
 void bmpoled::drawTemp(const int x)
 {
-    window << x << " C";
+    window << "\n" << x << " C";
     display.flush();
+    hwlib::wait_ms(100);
+    display.flush();
+}
+
+void bmpoled::readBMP()
+{
+//hier verder gaan. bus.write doen? ambiqious error? Kennelijk meerdere writes bekend? vragen?
 }
--- bmpoled.hpp
+++ bmpoled.hpp
@@ -6,16 +6,20 @@ class bmpoled
 private:
     hwlib::target::pin_oc sclOled;
     hwlib::target::pin_oc sdaOled;
-    hwlib::i2c_bus_bit_banged_scl_sda busOled;
+    hwlib::i2c_bus_bit_banged_scl_sda bus;
     hwlib::glcd_oled display;
 
     hwlib::font_default_16x16 font;
     hwlib::terminal_from window;
+
+    const uint8_t bmpAdress = 0b1110110;
     
 
 public:
     bmpoled(hwlib::target::pin_oc sclOled, hwlib::target::pin_oc sdaOled);
     void drawTemp(const int x);
+    void readBMP();
+    void writeBMP();
 };
 
 
