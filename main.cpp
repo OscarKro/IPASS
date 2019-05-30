63e0f112122b1b0ec0850f844b5b4faf8895861f
OscarKromhout <oscarkromhout@gmail.com>
2019-05-30 11:51:14 +0200

    .gitignore file aangemaakt en Oled scherm werkend

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 #include "hwlib.hpp"
 
 int main( void ){	  
-   namespace target = hwlib::target;
    
-   auto scl = target::pin_oc( target::pins::scl );
-   auto sda = target::pin_oc( target::pins::sda );
+   auto scl = hwlib::target::pin_oc( hwlib::target::pins::scl );
+   auto sda = hwlib::target::pin_oc( hwlib::target::pins::sda );
    
    auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl,sda );
    
    auto display = hwlib::glcd_oled( i2c_bus, 0x3c );  
      
-   display.clear();
-   for( uint16_t i = 10; i < display.size.x - 10; i++ ){
-       display.write( hwlib::location( i, 10 ), hwlib::black );
-   }
+   hwlib::graphics_random_circles( display, 5 );
 }
